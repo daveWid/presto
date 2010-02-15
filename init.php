9aadb3f89cd9604ccc8aad8dec4bd48d918a02ce
Dave Widmer <dave@davewidmer.net>
2010-02-15 11:47:59 -0500

    Adding Template controller and media loading.

---
+++ init.php
@@ -0,0 +1,9 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+// Media Loader routes
+Route::set('presto/media', 'media(/<file>)', array('file' => '.+'))
+	->defaults(array(
+		'controller' => 'presto_media',
+		'action'     => 'load',
+		'file'       => NULL,
+	));
