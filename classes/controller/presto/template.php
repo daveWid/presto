04364ce40d8c347449b0ee362affa6c4bb721b42
Dave Widmer <dave@davewidmer.net>
2011-02-17 12:25:40 -0500

    Cleaning up Template controller

--- classes/controller/presto/template.php
+++ classes/controller/presto/template.php
@@ -1,62 +1,63 @@
 <?php defined('SYSPATH') or die('No direct script access.');
+
 /**
- * The main template controller.
+ * The Presto Template controller.
+ * This will be used by all view that need to be wrapped into the template.
  *
- * @package	presto
- * @author	Dave Widmer
- * @copyright	2010 © Dave Widmer
+ * @package		Presto
+ * @author		Dave Widmer
+ * @copyright	2011 © Dave Widmer
  */
-class Controller_Presto_Template extends Kohana_Controller
+class Controller_Presto_Template extends Kohana_Controller_Template
 {
-	/** @var  string/View	Page template */
-	public $template = 'template';
+	/**
+	 * @var string	Page title
+	 */
+	public $title = "";
 
-	/** @var	boolean	Rend the template automatically? */
-	public $auto_render = TRUE;
+	/**
+	 * Add the path to the script you need.
+	 *
+	 * @var array	Extra script files to be included.
+	 */
+	public $js = array();
 
-	/** @var	array	List of CSS files */
+	/**
+	 * You will need to add the path => type into the array.
+	 * (e.g "reset.css" => "screen"
+	 *
+	 * @var array	Extra CSS to be included in the template
+	 */
 	public $css = array();
 
-	/** @var	array	List of Javascript files */
-	public $js  = array();
+	/**
+	 * You will need to add the property => value into the $meta array.
+	 *
+	 * @var array
+	 */
+	public $meta = array("charset" => "utf-8");
 
-	/** @var	string	Page Title */
-	public $title;
-
-	/** @var	View	View used as the main content of the page. */
+	/**
+	 * @var View	The view for the content
+	 */
 	public $content;
 
 	/**
-	 * Preloads the view based on the action
-	 */
-	public function before()
-	{
-		// If the auto render flag is set, load the template.
-		if ($this->auto_render === TRUE)
-		{
-			$this->template = View::factory($this->template);
-		}
-	}
-
-	/**
-	 * Completes the request
+	 * Adss variables to the template before it renders
 	 */
 	public function after()
 	{
-		// If auto_render then load up the content and display the template.
 		if ($this->auto_render === TRUE)
 		{
-			// Add in the css, js, title and view
-			$this->template->set( array(
-				'css' => $this->css,
-				'js' => $this->js,
-				'title' => $this->title,
-				'content' => $this->content,
-			));
+			$vars = array("title", "js", "css", "meta", "content");
 
-			// Render the response
-			$this->request->response = $this->template;
+			foreach ($vars as $key)
+			{
+				$this->template->set($key, $this->{$key});
+			}
 		}
+
+		return parent::after();
 	}
 
 }
