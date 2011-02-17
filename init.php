d3321c54dec378d01eed9ae82faefc15a0f888e2
Dave Widmer <dave@davewidmer.net>
2011-02-17 12:32:33 -0500

    Cleaning up (deleting) old files.

--- init.php
+++
@@ -1,9 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-// Media Loader routes
-Route::set('presto/media', 'media(/<file>)', array('file' => '.+'))
-	->defaults(array(
-		'controller' => 'presto_media',
-		'action'     => 'index',
-		'file'       => NULL,
-	));
